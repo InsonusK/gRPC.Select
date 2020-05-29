371969107faf5a47a7a22529dbe8f78daf8c70e7
InsonusK <insonus.k@gmail.com>
2020-05-30 01:18:42 +0300

    Add proto adapter

---
+++ gRPC.Select/gRPC.Select.TestInterface/Tests/Selector/SelectorRequest_Test.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Linq;
+using gRPC.Select.Exceptions;
+using gRPC.Select.Interface;
+using gRPC.Select.Test.TestTools;
+using NSubstitute;
+using NUnit.Framework;
+using Test;
+
+namespace gRPC.Select.Test.Tests.Selector
+{
+    public class SelectorRequest_Test
+    {
+        private DBInit _dbInit;
+        private IServiceProvider _serviceProvider;
+
+        [SetUp]
+        public void OnTimeSetup()
+        {
+            _dbInit = new DBInit(this.GetType().Name);
+            _dbInit.InitDB();
+            _serviceProvider = Substitute.For<IServiceProvider>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _dbInit.CleanupDB();
+        }
+
+        [Test]
+        public void WrongStream()
+        {
+            // Array
+            using var _context = new DBContext(_dbInit.DbContextOptions);
+            ISelector _selector = new Select.Selector(_serviceProvider);
+            var _selectRequest = new Another();
+            var _expectedResult = _context.Table.ToArray();
+
+            // Act
+            var _exception = Assert.Catch<ConditionException>(() =>
+                _selector.Apply(_context.Table.AsQueryable(), _selectRequest).ToArray());
+
+            // Assert
+            Assert.AreEqual("An expected type of condition message", _exception.Message);
+        }
+    }
+}
---
+++ gRPC.Select/gRPC.Select/Adapter/BaseProtoAdapter.cs
@@ -0,0 +1,19 @@
+using Google.Protobuf;
+using GRPC.Selector;
+
+namespace gRPC.Select.Adapter
+{
+    /// <summary>
+    /// Adapter for classes made from select.proto
+    /// </summary>
+    /// <typeparam name="TMessage">Proto message type</typeparam>
+    public class BaseProtoAdapter<TMessage> : IConditionAdapter<TMessage>
+    {
+        public SelectRequest Convert(TMessage message)
+        {
+            SelectRequest _selectRequest = new SelectRequest();
+            _selectRequest.MergeFrom(new CodedInputStream(((IMessage)message).ToByteArray()));
+            return _selectRequest;
+        }
+    }
+}
